7face317e2d269020a91570aa2824fccd0c3c42e
Georgy Savva <georgy.savva@nyu.edu>
2024-11-16 22:05:28 -0500

    make the script record videos

--- scripts/collect_doom_data.py
+++ scripts/collect_doom_data.py
@@ -1,6 +1,7 @@
 import argparse  # Add argparse import
 import os
 
+import cv2
 import gymnasium as gym
 import torch
 from tqdm import tqdm
@@ -10,15 +11,26 @@ from vizdoom import gymnasium_wrapper  # to register envs
 # Collect episodes using a random policy
 def collect_episodes(env, num_episodes, save_path):
     os.makedirs(save_path, exist_ok=True)
-
+    video_path = os.path.join(save_path, "videos")
+    os.makedirs(video_path, exist_ok=True)
     for episode in tqdm(range(num_episodes), desc="Sampling episodes"):
         episode_data = {"observations": [], "actions": [], "rewards": []}
         observation, _ = env.reset()
         done = False
         episode_reward = 0
+        video_filename = os.path.join(video_path, f"episode_{episode}.mp4")
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Use 'XVID' for .avi files
+        out = cv2.VideoWriter(
+            video_filename,
+            fourcc,
+            60,
+            (observation["screen"].shape[1], observation["screen"].shape[0]),
+        )
         while not done:
             # Random action
             action = env.action_space.sample()
+
+            out.write(observation["screen"])
 
             # Step environment
             (
@@ -48,6 +60,9 @@ def collect_episodes(env, num_episodes, save_path):
         )
         torch.save(episode_data, episode_file)
         print(f"Episode {episode} saved to {episode_file}")
+        # Close the video writer
+        out.release()
+        print(f"Episode {episode} video saved to {video_filename}")
 
 
 # Main function
