2c2ea0fc3edb22c373a21bc696562f6a4b58555a
Jack Lu <jacklu0831@gmail.com>
2024-11-29 14:02:36 -0500

    trained vae

--- bash_commands/1128_0_train_vae.sh
+++ bash_commands/1128_0_train_vae.sh
@@ -4,14 +4,14 @@
 # python src/vae/train_vae.py --tag test --learning_rate 1e-5 --max_train_steps 500 --checkpointing_steps 50 --n_eval_times 5 --wandb
 
 # train_vae_3e-6
-python src/vae/train_vae.py --tag 1128_0_train_vae_lr3e-6 --learning_rate 3e-6
+python src/vae/train_vae.py --tag 1128_0_train_vae_lr3e-6 --learning_rate 3e-6 --wandb
 
 # train_vae_1e-6
-python src/vae/train_vae.py --tag 1128_0_train_vae_lr1e-6 --learning_rate 1e-6
+python src/vae/train_vae.py --tag 1128_0_train_vae_lr1e-6 --learning_rate 1e-6 --wandb
 
-train_vae_1e-7
-python src/vae/train_vae.py --tag 1128_0_train_vae_lr1e-7 --learning_rate 1e-7
+# train_vae_1e-7
+python src/vae/train_vae.py --tag 1128_0_train_vae_lr1e-7 --learning_rate 1e-7 --wandb
 
-# Submitted batch job 54089263
-# Submitted batch job 54089264
-# Submitted batch job 54089273
+# Submitted batch job 54097636
+# Submitted batch job 54097637
+# Submitted batch job 54097638
