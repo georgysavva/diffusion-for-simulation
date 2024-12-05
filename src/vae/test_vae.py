57636c3b030e0d8df92021825f8f5c8775502d7a
Jack Lu <jacklu0831@gmail.com>
2024-12-05 18:26:31 -0500

    refactor

--- src/vae/test_vae.py
+++ src/vae/test_vae.py
@@ -4,10 +4,26 @@ from diffusers import AutoencoderKL
 import torch
 from PIL import Image
 from torchvision import transforms
+import copy
+
 
 # Load the VAE model
 vae = AutoencoderKL.from_pretrained('stabilityai/sd-vae-ft-ema').cuda()
 # vae = AutoencoderKL.from_pretrained('pt-sk/stable-diffusion-1.5', subfolder='vae').cuda()
+
+saved_params = {}
+for n, p in vae.named_parameters():
+    saved_params[n] = copy.deepcopy(p.data)
+
+vae.decoder.load_state_dict(torch.load('vae_output/1128_0_train_vae_lr3e-6/vae_25000.pth', weights_only=True))
+for n, p in vae.named_parameters():
+    if 'encoder' in n.lower():
+        assert torch.equal(saved_params[n], p.data)
+    elif 'decoder' in n.lower():
+        assert not torch.equal(saved_params[n], p.data)
+    else:
+        print(n)
+        print(torch.equal(saved_params[n], p.data))
 
 # Preprocess the input image
 image_path = 'image.png'
